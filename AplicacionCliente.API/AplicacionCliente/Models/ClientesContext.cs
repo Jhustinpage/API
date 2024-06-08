85726e21203d98334160bc3554cc8d67366d69d5
Jhustinpage <107010258+Jhustinpage@users.noreply.github.com>
2024-06-08 08:35:25 -0400

    Update ClientesContext.cs
    Comentarios añadidos.

--- AplicacionCliente.API/AplicacionCliente/Models/ClientesContext.cs
+++ AplicacionCliente.API/AplicacionCliente/Models/ClientesContext.cs
@@ -1,12 +1,13 @@
-﻿using Microsoft.EntityFrameworkCore;
+//La siguiente clase define el contexto de la base de datos para la gestión de clientes en la aplicación AplicacionCliente.
+using Microsoft.EntityFrameworkCore;    //Esta directiva de uso permite acceder a las clases y funcionalidades proporcionadas por la biblioteca Microsoft.EntityFrameworkCore, que es un framework de mapeo objeto-relacional (ORM) utilizado para interactuar con bases de datos en aplicaciones .NET.
 
-namespace AplicacionCliente.Models;
+namespace AplicacionCliente.Models;    //El espacio de nombres Models dentro de AplicacionCliente se utiliza para agrupar todos los modelos de datos relacionados con la aplicación.
 
-public class ClientesContext : DbContext
+public class ClientesContext : DbContext    //Clase que representa el contexto de la base de datos para la gestión de clientes en la aplicación.
 {
-    public ClientesContext(DbContextOptions<ClientesContext> options) : base(options) {
+    public ClientesContext(DbContextOptions<ClientesContext> options) : base(options) {    //Constructor de la clase que recibe las opciones de configuración del contexto de la base de datos.
     }
 
-    public DbSet<Clientes> Clientes {get; set;} = null!;
+    public DbSet<Clientes> Clientes {get; set;} = null!;    //Propiedad que representa una colección de clientes en la base de datos, que se mapea a la tabla de clientes en la base de datos.
 }
 
