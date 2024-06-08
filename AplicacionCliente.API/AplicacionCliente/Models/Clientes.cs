17da9aa62db66843cb6c75c3a760ad1a3091ab6f
Jhustinpage <107010258+Jhustinpage@users.noreply.github.com>
2024-06-08 08:30:23 -0400

    Update Clientes.cs
    Comentarios añadidos.

--- AplicacionCliente.API/AplicacionCliente/Models/Clientes.cs
+++ AplicacionCliente.API/AplicacionCliente/Models/Clientes.cs
@@ -1,10 +1,11 @@
-﻿namespace AplicacionCliente.Models
+//La siguiente clase define el modelo de datos para representar a un cliente en la aplicación AplicacionCliente.
+namespace AplicacionCliente.Models
 {
-    public class Clientes
+    public class Clientes    //Clase que representa el modelo de datos para un cliente en la aplicación.
     {
-        public int Id { get; set; }
-        public string? RazonSocial { get; set; }
-        public string Direccion {get; set; }
-        public string? Rut { get; set; }
+        public int Id { get; set; }    //Propiedad que representa el identificador único del cliente.
+        public string? RazonSocial { get; set; }    //Propiedad que representa la razón social del cliente.
+        public string Direccion {get; set; }    //Propiedad que representa la dirección del cliente.
+        public string? Rut { get; set; }    //Propiedad que representa el RUT del cliente, opcional.
     }
 }
